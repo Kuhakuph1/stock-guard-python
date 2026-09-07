6bae5c4a7eb38d01330a4314596e59b41a837433
Anggara Pangestu <anggaraph21@gmail.com>
2026-09-07 08:51:01 +0700

    fix ROLE_MAP error and update bot services

--- role_service.py
+++ role_service.py
@@ -3,70 +3,46 @@ from sheet_service import (
     add_user_role
 )
 
+# Inisialisasi variabel global ROLE_MAP di luar fungsi
+ROLE_MAP = None
+
+
 def get_all_users():
-
     data = get_user_role_data()
-
     users = []
 
     for row in data[1:]:
-
         if len(row) < 4:
             continue
 
         users.append({
-
             "id": row[1],
-
             "username": row[2],
-
             "role": row[3]
-
         })
 
     return users
 
-def get_user_role(
-    telegram_id
-):
 
-    data = get_user_role_data()
+def get_user_role(telegram_id):
+    global ROLE_MAP
 
-    for row in data[1:]:
+    if ROLE_MAP is None:
+        ROLE_MAP = {}
+        data = get_user_role_data()
 
-        if len(row) < 4:
-            continue
+        for row in data[1:]:
+            if len(row) < 4:
+                continue
 
-        if row[1] == str(
-            telegram_id
-        ):
+            ROLE_MAP[row[1]] = row[3]
 
-            return row[3]
-
-    return "VIEWER"
-
-def is_admin(
-    telegram_id
-):
-
-    return (
-        get_user_role(
-            telegram_id
-        )
-        == "ADMIN"
-    )
+    return ROLE_MAP.get(str(telegram_id), "VIEWER")
 
 
-def is_operator(
-    telegram_id
-):
+def is_admin(telegram_id):
+    return get_user_role(telegram_id) == "ADMIN"
 
-    return (
-        get_user_role(
-            telegram_id
-        )
-        in [
-            "ADMIN",
-            "OPERATOR"
-        ]
-    )
+
+def is_operator(telegram_id):
+    return get_user_role(telegram_id) in ["ADMIN", "OPERATOR"]
